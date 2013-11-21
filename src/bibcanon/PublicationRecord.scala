6e6646aa1470825cb3603f755fe9094ad891484a
Joe Neeman <joeneeman@gmail.com>
2013-11-20 18:55:28 -0600

    Add missing PublicationRecord.

---
+++ src/bibcanon/PublicationRecord.scala
@@ -0,0 +1,34 @@
+package bibcanon
+
+import bibtex.Name
+
+/**
+ * A record describing a publication.
+ * 
+ * This is different from a BibtexEntry, because BibtexEntry is tied
+ * to particular textural representations (e.g., of author names).
+ * 
+ * This is not a semantic structure, in the sense that we only
+ * give author and journal names, and not references to author
+ * and journal objects.
+ */
+trait PublicationRecord {
+  // Mandatory fields
+  def title: String
+  def authors: List[Name]
+  def publicationType: String
+  
+  // Optional fields
+  def year: Option[Int]
+  
+  /**
+   * Journal name, for example.
+   */
+  def containerTitle: Option[String]
+  
+  def pages: Option[Range]
+  def issue: Option[Int]
+  def volume: Option[Int]
+  def publisher: Option[String]
+  def doi: Option[String]
+}
